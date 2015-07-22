078d2b9536245b77744945d1993ca3464a7d61e8
Juan Manuel Silva Garretón <jmsg1990@gmail.com>
2015-07-22 16:39:40 -0300

    add post_type to search_index

--- djblog/search_indexes.py
+++ djblog/search_indexes.py
@@ -11,6 +11,8 @@ logger = logging.getLogger(__name__)
 class PostIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
+    post_type = indexes.CharField()
+
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return Post.objects.active_no_lang()
@@ -20,3 +22,6 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
     
     def get_model(self):
         return Post
+
+    def prepare_post_type(self, post):
+        return post.post_type.post_type_slug
