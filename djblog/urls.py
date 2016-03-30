5ec4ec684f48c4f3daee0fb07345d084a2cd5f20
Marco Puccio <mpuccio90@gmail.com>
2016-03-30 12:21:01 -0300

    fix url slash conditional

--- djblog/urls.py
+++ djblog/urls.py
@@ -34,43 +34,43 @@ if 'haystack' in getattr(settings, 'INSTALLED_APPS', []):
 #   [blog_tag_list]             /tag/slug/
 #   [blog_search_list]          /search/?q=QUERY+PARAMS
 urlpatterns += patterns('',
-    url(r'^blog/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^blog/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w\-\_\.]+)/$', 
         PostDateDetailView.as_view(), 
         name='blog_detail'), 
 
-    url(r'^blog/(?P<year>[0-9]{4})/?$', 
+    url(r'^blog/(?P<year>[0-9]{4})/$', 
         PostYearListView.as_view(), 
         name='blog_year_list'), 
 
-    url(r'^blog/(?P<year>\d{4})/(?P<month>\d{1,2})/?$', 
+    url(r'^blog/(?P<year>\d{4})/(?P<month>\d{1,2})/$', 
         PostMonthListView.as_view(), 
         name='blog_year_month_list'), 
 
-    url(r'^blog/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/?$', 
+    url(r'^blog/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 
         PostDayListView.as_view(), 
         name='blog_year_month_day_list'), 
 
-    url(r'^blog/thisweek/?$', 
+    url(r'^blog/thisweek/$', 
         PostWeekListView.as_view(), 
         name='blog_thisweek_list'),
 
-    url(r'^blog/today/?$', 
+    url(r'^blog/today/$', 
         PostWeekListView.as_view(), 
         name='blog_today_list'),
 
-    url(r'^blog/category/(?P<slug>[\w\-\_\.]+)/?$',
+    url(r'^blog/category/(?P<slug>[\w\-\_\.]+)/$',
         PostCategoryListView.as_view(), 
         name='blog_category_list'),
 
-    url(r'^blog/author/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^blog/author/(?P<slug>[\w\-\_\.]+)/$', 
         PostAuthorListView.as_view(), 
         name='blog_author_list'),
 
-    url(r'^blog/tag/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^blog/tag/(?P<slug>[\w\-\_\.]+)/$', 
         PostTagListView.as_view(), 
         name='blog_tag_list'),
 
-    url(r'^blog/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^blog/(?P<slug>[\w\-\_\.]+)/$', 
         PostDetailView.as_view(), 
         name='blog_canonical_detail'),
 
@@ -84,15 +84,15 @@ urlpatterns += patterns('',
 #   [page_hierarchy_detail]     /slug[_parent]/slug/
 #   [page_category_list]        /slug/
 urlpatterns += patterns('',
-    url(r'^page/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^page/(?P<slug>[\w\-\_\.]+)/$', 
         PageDetailView.as_view(), 
         name='page_detail'),
 
-    url(r'^page/category/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^page/category/(?P<slug>[\w\-\_\.]+)/$', 
         PageCategoryListView.as_view(), 
         name='page_category_list'),
 
-    url(r'^page/(?P<category_slug>[\w\-\_\.]+)/(?P<hierarchy_slug>[\w\-\_\./]+)$', 
+    url(r'^page/(?P<category_slug>[\w\-\_\.]+)/(?P<hierarchy_slug>[\w\-\_\./]+)', 
         PageHierarchyDetailView.as_view(), 
         name='page_hierarchy_detail'),
 
@@ -100,15 +100,15 @@ urlpatterns += patterns('',
 
 # Custom URLs
 urlpatterns += patterns('',
-    url(r'^(?P<post_type_slug>[\w\-\_\.]+)/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^(?P<post_type_slug>[\w\-\_\.]+)/(?P<slug>[\w\-\_\.]+)/$', 
         GenericPostDetailView.as_view(), 
         name='generic_post_detail'),
 
-    url(r'^(?P<post_type_slug>[\w\-\_\.]+)/?$', 
+    url(r'^(?P<post_type_slug>[\w\-\_\.]+)/$', 
         GenericPostListView.as_view(), 
         name='generic_post_list'),
 
-    url(r'^(?P<post_type_slug>[\w\-\_\.]+)/category/(?P<slug>[\w\-\_\.]+)/?$', 
+    url(r'^(?P<post_type_slug>[\w\-\_\.]+)/category/(?P<slug>[\w\-\_\.]+)/$', 
         GenericCategoryListView.as_view(), 
         name='generic_category_list'),
 )
